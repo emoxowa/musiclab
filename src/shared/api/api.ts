00960988af71b03942d14337d7a758f841dafce5
Evgeniia <evgeniia@MacBook-Pro-Evgeniia.local>
2026-08-12 12:56:25 +0300

    chore: add Prettier

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,9 @@
 import { API_URL } from '@/shared/config/env'
 
-export async function apiFetch<T>(path: string, init?: RequestInit): Promise<T> {
+export async function apiFetch<T>(
+  path: string,
+  init?: RequestInit,
+): Promise<T> {
   const response = await fetch(`${API_URL}${path}`, {
     credentials: 'include',
     ...init,
